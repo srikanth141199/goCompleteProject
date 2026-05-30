ef23ec2e1053571da7618e3db341efcc5c73da3b
Srikanth K <kolleparasrikanth@gmail.com>
2026-05-30 20:36:00 +0530

    implemented user id linkage to event creation through jwt token

--- models/event.go
+++ models/event.go
@@ -11,7 +11,7 @@ type Event struct {
 	Description string    `json:"description" binding:"required"`
 	Location    string    `json:"location" binding:"required"`
 	DateTime    time.Time `json:"dateTime" binding:"required"`
-	UserID      int
+	UserID      int64
 }
 
 var events = []Event{}
--- utils/jwt.go
+++ utils/jwt.go
@@ -20,7 +20,7 @@ func GenerateToken(email string, userId int64) (string, error) {
 
 }
 
-func VerifyToken(token string) error {
+func VerifyToken(token string) (int64, error) {
 	parsedToken, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
 		_, ok := token.Method.(*jwt.SigningMethodHMAC)
 		if !ok {
@@ -30,21 +30,21 @@ func VerifyToken(token string) error {
 	})
 
 	if err != nil {
-		return errors.New("Could not parse Token.")
+		return 0, errors.New("Could not parse Token.")
 	}
 
 	tokenIsValid := parsedToken.Valid
 	if !tokenIsValid {
-		return errors.New("Token is not Valid, kindly signIn again")
+		return 0, errors.New("Token is not Valid, kindly signIn again")
 	}
-	// claims, ok := parsedToken.Claims.(jwt.MapClaims)
+	claims, ok := parsedToken.Claims.(jwt.MapClaims)
 
-	// if !ok {
-	// 	return errors.New("Token is not Valid, kindly signIn again")
-	// }
+	if !ok {
+		return 0, errors.New("Token is not Valid, kindly signIn again")
+	}
 
-	// email := claims["email"].(string)
-	// userId := claims["userId"].(int64)
+	//email := claims["email"].(string)
+	userId := int64(claims["userId"].(float64))
 
-	return nil
+	return userId, nil
 }
