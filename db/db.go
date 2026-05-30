7fcd0e51e5f0326f4c7c6b7cc021df23f2662833
Srikanth K <kolleparasrikanth@gmail.com>
2026-05-30 23:45:16 +0530

    created a new Table for registrations

--- db/db.go
+++ db/db.go
@@ -55,4 +55,19 @@ func createTables() {
 	if err != nil {
 		panic("Could not create events table: " + err.Error())
 	}
+
+	createRegistrationTable := `
+		CREATE TABLE IF NOT EXISTS registrations (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			event_id INTEGER,
+			user_id INTEGER,
+			FOREIGN KEY(event_id) REFERENCES events(id),
+			FOREIGN KEY(user_id) REFERENCES users(id)
+		)
+	`
+
+	_, err = DB.Exec(createRegistrationTable)
+	if err != nil {
+		panic("Could not create Registrations table: " + err.Error())
+	}
 }
