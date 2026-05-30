ce747da996cb02bc9c18c29382aee7c3fc70412a
Srikanth K <kolleparasrikanth@gmail.com>
2026-05-31 00:14:23 +0530

    implemented cancellation of registration

--- routes/register.go
+++ routes/register.go
@@ -34,5 +34,21 @@ func registerForEvent(context *gin.Context) {
 }
 
 func cancelRegistration(context *gin.Context) {
+	userId := context.GetInt64("userId")
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Failed to parse event ID"})
+		return
+	}
 
+	var event models.Event
+
+	event.ID = eventId
+	err = event.CancelRegistration(userId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not cancel registration for Event"})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"message": "Registration has been successfully canceled"})
 }
